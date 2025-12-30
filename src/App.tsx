ed7dac67b98506d8fd6aec87c6d689c42d19aa4c
vermeer314 <vermeer314@gmail.com>
2025-12-31 00:44:11 +0900

    feat: add .env and game type

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+const API_KEY = import.meta.env.VITE_RAWG_API_KEY;
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    axios
+      .get(`https://api.rawg.io/api/games?key=${API_KEY}`)
+      .then((res) => {
+        console.log('게임 리스트:', res.data.results);
+      })
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Game Archive</h1>
+      <p>콘솔창을 확인해 보세요!</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/types/game.ts
@@ -0,0 +1,19 @@
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  released: string;
+  rating: number;
+  metacritics: number;
+  playtime: number;
+  platform: Platform;
+  //버전이 다른 플랫폼들을 동일한 플랫폼으로 묶어주기
+  //ex) PlayStation 3, 4, 5 >> PlayStation
+  parent_platforms: { platform: Platform }[];
+}
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
