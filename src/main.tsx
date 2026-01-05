04500a07748251a59e4dfa198ca0bf67b45762c1
vermeer314 <vermeer314@gmail.com>
2026-01-06 03:26:23 +0900

    feat: deploy to github pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/game-archive">
       <App />
     </BrowserRouter>
   </StrictMode>
